1529d4f5fec331690dbcdbdb62b7a9f3ff333564
Nairen Fu <nairenf2@illinois.edu>
2021-04-11 00:13:04 -0500

    Implemented basic room joining and hand for FE, and play on backend.

---
+++ cards/src/card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { playCard } from './websocket/socket.js'
+
+export default class Card extends React.Component {
+  handleClick = (event) => {
+    playCard(this.props.uid, this.props.rid, this.props.onCardPlayed)
+  }
+
+  render() {
+    return (
+      <div className='card noselect' onClick={this.handleClick}>
+        {this.props.suit} {this.props.value}
+      </div>
+    )
+  }
+}
