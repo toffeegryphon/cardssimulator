488b66be6732f4452d7a3c3840304319ecd3ebe2
sanjiviyer <sanjivi2@illinois.edu>
2021-04-11 02:30:06 -0500

    add deal

--- server/__main__.py
+++ server/__main__.py
@@ -3,14 +3,17 @@
 
 from aiohttp import web
 import socketio
+from ..core.game_instance import GameInstance
 
 sio = socketio.AsyncServer(cors_allowed_origins='*')
 app = web.Application()
 sio.attach(app)
+playerList = {}
 
 @sio.event
 def connect(sid, environ):
     print("connect ", sid)
+    
 
 @sio.event
 async def chat_message(sid, data):
@@ -23,13 +26,27 @@ def disconnect(sid):
 @sio.on('join')
 def on_join(sid, rid: str):
     sio.enter_room(sid, rid)
-    return rid
+    playerList.setdefault(rid, { 'players': [], 'instance': GameInstance()})['player'].append(sid)
 
 @sio.on('play')
 async def on_play(sid, data: dict):
     print(data)
     await sio.emit('update', { 'action': 'add', **data }, room=data['rid'])
     return { 'action': 'remove', 'value': data['value']}
+
+@sio.on('shuffle')
+async def on_shuffle(sid, data: dict):
+    
+         
+            
+    
+    
+
+@sio.on('deal')
+async def on_deal(sid, data: dict):
+    for i in playerList[data['rid']]['players']:
+        await sio.emit('update', { 'action': 'add', **data }, room= i)
+    
 
 #  app.router.add_static('/static', 'static')
 #  app.router.add_get('/', index)
