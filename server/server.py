e5d77df3ed674ab66c768c9803bd9364fe581bc4
Nairen Fu <nairenf2@illinois.edu>
2021-04-11 05:05:08 -0500

    Implemented draw on frontend.

--- server/server.py
+++ server/server.py
@@ -67,10 +67,12 @@ async def on_shuffle(sid, data: dict):
 async def on_deal(sid, data: dict):
     print(playerList)
     rid = data['rid']
-    broadcast = get_instance(rid).deal(data['count'])
+    broadcast, state = get_instance(rid).deal(data['count'])
     print(broadcast)
-    for pid in get_players(rid):
-        await sio.emit('update', { 'action': 'add', **data }, room=rid)
+    for pid in broadcast:
+        message = broadcast[pid]
+        message['state'] = state
+        await sio.emit('update', message, room=rid)
     
 
 #  app.router.add_static('/static', 'static')
