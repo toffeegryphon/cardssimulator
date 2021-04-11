a9b4f831eb4f0d53280c34224184d2ef7f4ea8b0
Param Mehta <paramum2@illinois.edu>
2021-04-11 00:40:06 -0500

    Added Movement of Cards for The Play Function

--- shuffleanddeal.py
+++ shuffleanddeal.py
@@ -24,3 +24,19 @@ class GameInstance:
             removed_value = random.choice(deck.hand)
             deck.remove(removed_value)
             player.hand.append(removed_value)
+
+    #First is a boolean (if true, then player 1 is giving the card to player 2)
+    def play(self, player1, player2, field, deck, first):
+        if (player2 == None):
+            players_card = player1.Card
+            player1.hand.remove(players_card)
+            field.append(players_card)
+        elif (first):
+            move_card(player1, player2)
+        else:
+            move_card(player2, player1)
+
+    def move_card(player1, player2):
+        players_card = player1.Card
+        player1.hand.remove(players_card)
+        player2.append(players_card)
