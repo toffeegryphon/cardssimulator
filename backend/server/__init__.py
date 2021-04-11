f07d741fe757ed4b2874160c5267c81799d8f7b1
Nairen Fu <nairenf2@illinois.edu>
2021-04-11 09:56:57 -0500

    Logging.

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -4,7 +4,8 @@
 import socketio
 import tornado
 
-sio = socketio.AsyncServer(async_mode='tornado', cors_allowed_origins='*')
+sio = socketio.AsyncServer(async_mode='tornado', cors_allowed_origins='*',
+                           logger=True, engineio_logger=True)
 
 app = tornado.web.Application(
     [
