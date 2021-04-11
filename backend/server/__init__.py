e67f502fa241f637cbabf75ef4af08d85e0a7c7b
Nairen Fu <nairenf2@illinois.edu>
2021-04-11 07:11:40 -0500

    Changed init.

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 
 #  from . import server
+import socketio
+#  from server.core.game_instance import GameInstance
+
+sio = socketio.Server(async_mode='eventlet')
+app = socketio.WSGIApp(sio)
+import eventlet
+eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
